785d35cda35a601690ca1a2f95e79efe7a6b7821
Amin <dmezghanni@gmail.com>
2021-05-03 21:58:20 +0100

    changed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="allegro5",
-    version="1.0.5",
+    version="1.0.6",
     author="Amin Guermazi",
     author_email="mino260806@gmail.com",
     description="Allegro 5 game development library (originaly in C) wrapped in Python",
